f1845b6e556108c4275a492ce7cd5fe994d77b8b
Thalita F Drumond <thalitafdrumond@gmail.com>
2018-07-05 01:10:09 +0200

    support to reading miniimagenet

--- protonets/utils/data.py
+++ protonets/utils/data.py
@@ -3,6 +3,8 @@ import protonets.data
 def load(opt, splits):
     if opt['data.dataset'] == 'omniglot':
         ds = protonets.data.omniglot.load(opt, splits)
+    elif opt['data.dataset'].lower() == 'miniimagenet':
+        ds = protonets.data.miniImagenet.load(opt, splits)
     else:
         raise ValueError("Unknown dataset: {:s}".format(opt['data.dataset']))
 
